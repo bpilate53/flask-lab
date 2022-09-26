eb881586c2d66705bd4bf5ee752751c1b17564fa
Mahesh Sunuwar <ms@MacBook.local>
2022-09-26 02:05:30 -0500

    [x] crud funtionality added

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,6 @@
-from urllib import request
+from random import random
 import uuid
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from db import Database
 
 class Task:
@@ -9,18 +9,68 @@ class Task:
 
     def create(self):
         # print(request.form)
-        # task = {
-        #     "_id" : uuid.uuid4().hex,
-        #     "name": request.form.get('name'),
-        #     "user_id": ""
-        # }
-
         task = {
-            "name": "request.form.get('name')",
+            "name": request.form.get('taskname'),
             "user_id": ""
         }
 
-        self.todo_db.tasks.insert_one(task)
+        # task = {
+        #     "name": "task" + uuid.uuid4().hex,
+        #     "user_id": ""
+        # }
 
-        return jsonify(task), 200
+        # check for existing task
+        try: 
+            self.todo_db.tasks.insert_one(task)
+        except:
+            return {'error': 'Cannot delete.'}
+        else:
+            return {'error': 'Cannot delete.'}
 
+        return {'error': 'Cannot delete.'}
+        
+    def update(self):
+        update_task_name = request.form.get('taskname')
+        if update_task_name:
+            try:
+                self.todo_db.tasks.update_one({'name':request.form.get('oldname')}, {'$set': {'name': update_task_name}})
+                return self.get_one(update_task_name)
+            except:
+                return {'error': 'cannot update'}
+        else:
+            return {'error': 'cannot update'}
+    
+    def delete(self, name):
+        task_exist = self.task_exist(name)
+
+        if task_exist:
+            try:
+                self.todo_db.tasks.delete_one({'name': name})
+            except:
+                return {'error': 'Cannot delete.'}
+            else:
+                return {'error': 'Cannot delete.'}
+            
+        return {'error': 'Cannot delete.'}
+
+    def get_all(self):
+            all_tasks = self.todo_db.tasks.find({})
+            if all_tasks:
+                return list(all_tasks)
+            else:
+                return {'error': 'No tasks available'}
+    
+    def get_one(self, name):
+        task = list(self.todo_db.tasks.find({'name': name}))
+
+        if len(list(task)) > 0:
+            return (list(task))[0]
+        return {'error': 'No task found'}
+    
+    def task_exist(self, name):
+        task_exist = self.get_one(name)
+
+        if 'error' in task_exist:
+            return False
+        return True
+            
--- todo/routes.py
+++ todo/routes.py
@@ -1,13 +1,36 @@
-from crypt import methods
-from flask import Flask, Blueprint, render_template, url_for
+from flask import Flask, Blueprint, render_template, url_for, json, request, redirect
 from todo.models import Task
 
 bp = Blueprint('todo', __name__)
 
 @bp.route('/')
 def index():
-    return render_template('todo/index.html')
+    all_tasks = Task().get_all()
+    return render_template('todo/index.html', tasks = all_tasks)
 
-@bp.route('/create', methods=['GET'])
+@bp.route('/create', methods=['GET', 'POST'])
 def create():
-    return Task().create()
+    if request.method == 'POST':
+        new_task = Task().create()
+        return redirect(url_for('todo.index'))
+
+    return render_template('todo/create.html')
+    
+
+
+@bp.route('/update/<name>', methods=['GET', 'POST'])
+def update(name):
+    task = Task().get_one(name)
+    
+    if request.method == 'POST':
+        task = Task().update()
+        return redirect(url_for('todo.index'))
+        
+    return render_template('todo/update.html', task = task)
+
+@bp.route('/delete/<name>', methods=['POST'])
+def delete(name):
+    delete = Task().delete(name)
+    return redirect(url_for('todo.index'))
+
+    
